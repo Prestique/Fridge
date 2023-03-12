0b7580a492f4d8f6e58e83f1f3e50ec2ad0978ac
tbejszweda <j.szweda@t-bull.com>
2023-03-12 16:41:40 +0100

    kitchen v0.01

--- app/src/main/java/com/jszweda/fridge/MainActivity.java
+++ app/src/main/java/com/jszweda/fridge/MainActivity.java
@@ -3,12 +3,33 @@ package com.jszweda.fridge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textMain;
+    Button buttonFridge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textMain = findViewById(R.id.textViewMain);
+        textMain.setText("");
+
+        buttonFridge = findViewById(R.id.fridge);
+        buttonFridge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textMain.length() > 80){
+                    textMain.setText("");
+                }
+                textMain.append(" Clicked");
+            }
+        });
+
     }
 }
